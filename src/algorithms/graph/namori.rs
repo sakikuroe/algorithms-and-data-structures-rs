746b3d3db1116dbb2a9927ec60aa54b5d927cf3a
sakikuroe <sakikuroe96@gmail.com>
2022-11-13 16:55:49 +0900

    Fix implementation

--- src/algorithms/graph/namori.rs
+++ src/algorithms/graph/namori.rs
@@ -2,10 +2,10 @@ use crate::data_structures::graph::Graph;
 use std::collections::VecDeque;
 
 impl Graph<usize> {
-    pub fn cycle(&self) -> Vec<usize> {
+    fn cycle(&self) -> Vec<usize> {
         let mut indeg = self.edges.iter().map(|e| e.len()).collect::<Vec<_>>();
         let mut que = (0..self.len())
-            .filter(|i| indeg[*i] == 1)
+            .filter(|&i| indeg[i] == 1)
             .map(|i| (i, None))
             .collect::<VecDeque<_>>();
 
@@ -19,24 +19,38 @@ impl Graph<usize> {
             }
         }
 
-        (0..self.len())
-            .filter(|i| indeg[*i] == 2)
-            .collect::<Vec<_>>()
+        let mut res = vec![];
+        let mut visited = vec![false; self.len()];
+        let mut v = (0..).find(|&i| indeg[i] == 2).unwrap();
+        loop {
+            if visited[v] {
+                break;
+            }
+            res.push(v);
+            visited[v] = true;
+            for e in &self.edges[v] {
+                if indeg[e.dst] == 2 && !visited[e.dst] {
+                    v = e.dst;
+                    break;
+                }
+            }
+        }
+
+        res
     }
 
     /// verified by
     /// - AtCoder | [AtCoder Beginner Contest 266  F - Well-defined Path Queries on a Namori](https://atcoder.jp/contests/abc266/tasks/abc266_f) ([submittion](https://atcoder.jp/contests/abc266/submissions/34414332))
-    pub fn namori(&self) -> (Vec<usize>, Vec<(usize, Vec<usize>)>) {
+    pub fn namori(&self) -> Vec<Vec<usize>> {
         let face = self.cycle();
         let mut tentacles = vec![];
         let mut visited = vec![false; self.len()];
         face.iter().for_each(|i| visited[*i] = true);
 
-        for root in face.iter() {
-            let mut tentacle = vec![];
+        for &root in face.iter() {
+            let mut tentacle = vec![root];
             let mut que = VecDeque::new();
-            que.push_back(*root);
-            visited[*root] = true;
+            que.push_back(root);
             while let Some(u) = que.pop_front() {
                 for e in self.edges[u].iter() {
                     if visited[e.dst] {
@@ -47,9 +61,9 @@ impl Graph<usize> {
                     visited[e.dst] = true;
                 }
             }
-            tentacles.push((*root, tentacle));
+            tentacles.push(tentacle);
         }
 
-        (face, tentacles)
+        tentacles
     }
 }
