6baf7767a13d2946450de67310ffd6317dd1903e
sakikuroe <sakikuroe96@gmail.com>
2022-12-25 09:47:44 +0900

    Fix implementation

--- src/algorithms/graph/kruskal.rs
+++ src/algorithms/graph/kruskal.rs
@@ -8,9 +8,8 @@ where
     T: Clone + Copy + Ord,
 {
     //! verified by
-    //! - Aizu Online Judge | [AOJ GRL_2_A 最小全域木](https://onlinejudge.u-aizu.ac.jp/problems/GRL_2_A) ([submittion](https://onlinejudge.u-aizu.ac.jp/status/users/Kurosaki96/submissions/1/GRL_2_A/judge/6915986/Rust))
-    pub fn kruskal(&self) -> Vec<Edge<T>> {
-        let mut res = vec![];
+    //! - Aizu Online Judge | [AOJ GRL_2_A 最小全域木](https://onlinejudge.u-aizu.ac.jp/problems/GRL_2_A) ([submittion](https://onlinejudge.u-aizu.ac.jp/status/users/Kurosaki96/submissions/1/GRL_2_A/judge/7242144/Rust))
+    pub fn kruskal(&self) -> Graph<T> {
         let mut uf = UnionFind::new(self.len());
 
         let mut all_edges = self
@@ -22,10 +21,12 @@ where
 
         all_edges.sort_by_key(|e| e.weight);
 
+        let mut res = Graph::new(self.len());
+
         for e in all_edges {
             if !uf.is_same(e.src, e.dst) {
                 uf.union(e.src, e.dst);
-                res.push(e);
+                res.add_edge(e.src, e.dst,e.weight);
             }
         }
 
