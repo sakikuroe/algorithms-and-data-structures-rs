7556f569a6e1a03e2d8f9247a547eb728a1a54a6
sakikuroe <sakikuroe96@gmail.com>
2022-12-26 11:43:14 +0900

    Fix implementation

--- src/data_structures/fenwick_tree.rs
+++ src/data_structures/fenwick_tree.rs
@@ -3,7 +3,6 @@ pub struct FenwickTree {
     data: Vec<usize>,
 }
 
-/// *CAUTION* 1-based numbering
 impl FenwickTree {
     pub fn new(size: usize) -> FenwickTree {
         FenwickTree {
@@ -13,20 +12,25 @@ impl FenwickTree {
     }
 
     pub fn add(&mut self, mut idx: usize, x: usize) {
+        idx += 1;
         while idx <= self.size {
             self.data[idx] += x;
             idx += idx & (!idx + 1);
         }
     }
 
-    // return data[1] + ... + data[idx];
-    pub fn sum(&self, mut idx: usize) -> usize {
-        let mut res = 0;
-        while idx > 0 {
-            res += self.data[idx];
-            idx -= idx & (!idx + 1);
-        }
+    // return sum of data[l..r];
+    pub fn sum(&self, l: usize, r: usize) -> usize {
+        // return sum of data[0..r];
+        let accum = |mut idx: usize| {
+            let mut res = 0;
+            while idx > 0 {
+                res += self.data[idx];
+                idx -= idx & (!idx + 1);
+            }
+            res
+        };
 
-        res
+        accum(r) - accum(l)
     }
 }
